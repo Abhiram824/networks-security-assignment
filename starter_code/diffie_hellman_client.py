128718eb17c475d8e271c88d9c6aee3391eedbb9
Abhiram824 <abhisuhaas1@gmail.com>
2024-11-24 17:49:07 -0600

    initial implementation of dh keyex

--- starter_code/diffie_hellman_client.py
+++ starter_code/diffie_hellman_client.py
@@ -4,32 +4,47 @@ import argparse
 import random
 from pathlib import Path
 from typing import Tuple
+import socket
+import json
 
-
+MESSAGE_SIZE = 1024
 # TODO feel free to use this helper or not
 def send_common_info(sock: socket.socket, server_address: str, server_port: int) -> Tuple[int, int]:
     # TODO: Connect to the server and propose a base number and prime
     # TODO: You can generate these randomly, or just use a fixed set
     # TODO: Return the tuple (base, prime modulus)
+    BASE = 19
+    PRIME_MODULUS = 797
+    sock.connect((server_address, server_port))
+    data = {}
+    data['base'] = BASE
+    data['prime_modulus'] = PRIME_MODULUS
+    sock.sendall(json.dumps(data).encode())
+    return BASE, PRIME_MODULUS
     pass
 
 # Do NOT modify this function signature, it will be used by the autograder
 def dh_exchange_client(server_address: str, server_port: int) -> Tuple[int, int, int, int]:
     # TODO: Create a socket 
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     
     # TODO: Send the proposed base and modulus number to the server using send_common_info
-
+    base, prime_modulus = send_common_info(sock, server_address, server_port)
     # TODO: Come up with a random secret key
-
+    alice_secret_key = random.randint(1, 100)
     # TODO: Calculate the message the client sends using the secret integer.
-
+    alice_pk = (base ** alice_secret_key) % prime_modulus
+    alice_message = {}
+    alice_message['public_key'] = alice_pk
     # TODO: Exhange messages with the server
-
+    sock.sendall(json.dumps(alice_message).encode())
+    bob_info = sock.recv(MESSAGE_SIZE).decode()
+    bob_info = json.loads(bob_info)
     # TODO: Calculate the secret using your own secret key and server message
-    
+    shared_secret = (bob_info['public_key'] ** alice_secret_key) % prime_modulus
     # TODO: Return the base number, the modulus, the private key, and the shared secret
 
-    pass
+    return base, prime_modulus, alice_secret_key, shared_secret
 
 
 def main(args):
--- starter_code/diffie_hellman_server.py
+++ starter_code/diffie_hellman_server.py
@@ -4,8 +4,9 @@ import argparse
 import random
 from pathlib import Path
 from typing import Tuple
+import json
 
-
+MESSAGE_SIZE = 1024
 # TODO feel free to use this helper or not
 def receive_common_info() -> Tuple[int, int]:
     # TODO: Wait for a client message that sends a base number.
@@ -15,17 +16,31 @@ def receive_common_info() -> Tuple[int, int]:
 # Do NOT modify this function signature, it will be used by the autograder
 def dh_exchange_server(server_address: str, server_port: int) -> Tuple[int, int, int, int]:
     # TODO: Create a server socket. can be UDP or TCP.
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind((server_address, server_port))
+    sock.listen(1)
+    conn, addr = sock.accept()
 
     # TODO: Read client's proposal for base and modulus using receive_common_info
-
+    data = conn.recv(MESSAGE_SIZE).decode()
+    data = json.loads(data)
+    base = data['base']
+    prime_modulus = data['prime_modulus']
     # TODO: Generate your own secret key
-
+    bob_secret_key = random.randint(1, 100)
+    bob_pk = (base ** bob_secret_key) % prime_modulus
     # TODO: Exchange messages with the client
+    bob_message = {}
+    bob_message['public_key'] = bob_pk
+    sock.sendto(json.dumps(bob_message).encode(), addr)
+    
+    alice_info = conn.recv(MESSAGE_SIZE).decode()
+    alice_pk = json.loads(alice_info)["public_key"]
 
     # TODO: Compute the shared secret.
-
+    shared_secret = (alice_pk ** bob_secret_key) % prime_modulus
     # TODO: Return the base number, prime modulus, the secret integer, and the shared secret
-    pass
+    return base, prime_modulus, bob_secret_key, shared_secret
 
 def main(args):
     dh_exchange_server(args.address, args.port)
